79c58af008155627c935b8fc1d0a97f313b27984
aymericbarge <aymeric-barge@hotmail.fr>
2023-10-04 10:01:18 +0200

    Le filtre fonctionne

--- src/Controller/FiltreController.php
+++ src/Controller/FiltreController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Entity\Rayon;
 use App\Repository\RayonRepository;
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class FiltreController extends AbstractController
 {
     #[Route('/filtre', name: 'filtre')]
-    public function filtre(Request $request, RayonRepository $rayonRepository): Response
+    public function filtre(Request $request, RayonRepository $rayonRepository, ProduitRepository $produitRepository): Response
     {
         // Récupérez la liste des rayons
         $rayons = $rayonRepository->findAll();
@@ -19,24 +20,12 @@ class FiltreController extends AbstractController
         // Récupérez le rayon sélectionné (s'il y en a un)
         $selectedRayonId = $request->query->get('categorie');
 
-        // Utilisez var_dump pour afficher la valeur de $selectedRayonId
-        // var_dump($selectedRayonId);
-
         // Récupérez les produits liés au rayon sélectionné
         $produitsParRayon = [];
         if ($selectedRayonId) {
-            $rayon = $rayonRepository->find($selectedRayonId);
-
-            // Utilisez var_dump pour afficher le contenu de $rayon
-            // var_dump($rayon);
-
-            if ($rayon) {
-                $produitsParRayon[$selectedRayonId] = $rayon->getProduits();
-            }
+            // Utilisez le ProduitRepository pour récupérer les produits liés à la catégorie
+            $produitsParRayon[$selectedRayonId] = $produitRepository->findByCategorie($selectedRayonId);
         }
-
-        // Utilisez var_dump pour afficher le contenu de $produitsParRayon
-        // var_dump($produitsParRayon);
 
         return $this->render('filtre.html.twig', [
             'categories' => $rayons,
