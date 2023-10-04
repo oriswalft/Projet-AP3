a8b1ea286b81ee29d7645ff34e95491fc75a577e
aymericbarge <aymeric-barge@hotmail.fr>
2023-10-04 09:21:06 +0200

    Modification du filtre (n'affiche toujour pas les produits)

--- src/Controller/FiltreController.php
+++ src/Controller/FiltreController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Rayon;
 use App\Repository\RayonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,20 +13,30 @@ class FiltreController extends AbstractController
     #[Route('/filtre', name: 'filtre')]
     public function filtre(Request $request, RayonRepository $rayonRepository): Response
     {
+        // Récupérez la liste des rayons
         $rayons = $rayonRepository->findAll();
 
+        // Récupérez le rayon sélectionné (s'il y en a un)
         $selectedRayonId = $request->query->get('categorie');
 
+        // Utilisez var_dump pour afficher la valeur de $selectedRayonId
+        // var_dump($selectedRayonId);
+
+        // Récupérez les produits liés au rayon sélectionné
+        $produitsParRayon = [];
         if ($selectedRayonId) {
             $rayon = $rayonRepository->find($selectedRayonId);
+
+            // Utilisez var_dump pour afficher le contenu de $rayon
+            // var_dump($rayon);
+
             if ($rayon) {
-                $produitsParRayon = [$selectedRayonId => $rayon->getProduits()];
-            } else {
-                $produitsParRayon = [];
+                $produitsParRayon[$selectedRayonId] = $rayon->getProduits();
             }
-        } else {
-            $produitsParRayon = [];
         }
+
+        // Utilisez var_dump pour afficher le contenu de $produitsParRayon
+        // var_dump($produitsParRayon);
 
         return $this->render('filtre.html.twig', [
             'categories' => $rayons,
