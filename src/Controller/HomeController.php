9077db6e5d9fb88ca7b437ee36246e7eafedd279
aymericbarge <aymeric-barge@hotmail.fr>
2023-10-12 14:40:39 +0200

    Ajout du filtre sur l'accueil  + supression du controller du filtre

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,20 +5,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Rayon;
+use App\Repository\RayonRepository;
 use App\Repository\ProduitRepository;
 use App\Entity\Produit;
 use Symfony\Component\HttpFoundation\Request;
-use App\Form\InscriptionType;
+
 
 class HomeController extends AbstractController
 {
 
     #[Route('/', name: 'app_accueil')]
-    public function accueil(ProduitRepository $produitReposotory): Response
+    public function accueil(Request $request, RayonRepository $rayonRepository, ProduitRepository $produitRepository): Response
     {
-        $produits = $produitReposotory->findall();
+        $produits = $produitRepository->findall();
+        // Récupérez la liste des rayons
+        $rayons = $rayonRepository->findAll();
+
+        // Récupérez le rayon sélectionné (s'il y en a un)
+        $selectedRayonId = $request->query->get('categorie');
+
+        // Récupérez les produits liés au rayon sélectionné
+        $produitsParRayon = [];
+        if ($selectedRayonId) {
+            // Utilisez le ProduitRepository pour récupérer les produits liés à la catégorie
+            $produitsParRayon[$selectedRayonId] = $produitRepository->findByCategorie($selectedRayonId);
+        }
         return $this->render('home/accueil.html.twig', [
             'produits'  => $produits,
+            'categories' => $rayons,
+            'selectedRayonId' => $selectedRayonId,
+            'produitsParRayon' => $produitsParRayon,
         ]);
         
     }
@@ -27,17 +44,6 @@ class HomeController extends AbstractController
     {
         return $this->render('home/detail.html.twig', [
             'produit' => $produit,
-        ]);
-    }
-    #[Route('/inscription', name: 'inscription')]
-    public function inscription(Request $request): Response
-    {
-        $form = $this->createForm(InscriptionType::class);
-        // Gérez la soumission du formulaire si nécessaire
-        $form->handleRequest($request);
-        // Affichez le formulaire dans votre template
-        return $this->render('home/inscription.html.twig', [
-            'form' => $form->createView(),
         ]);
     }
 }
