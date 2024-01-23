c5b7f1cac93a9f6b36b72b39074fac0399792d6a
OrisWalft <constantchx20@gmail.com>
2024-01-23 15:44:17 +0100

    Ajout de API Platform

--- src/Entity/Enfant.php
+++ src/Entity/Enfant.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\EnfantRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EnfantRepository::class)]
+#[ApiResource()]
 class Enfant
 {
     #[ORM\Id]
--- src/Entity/Entrepot.php
+++ src/Entity/Entrepot.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\EntrepotRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -9,6 +10,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EntrepotRepository::class)]
+#[ApiResource()]
 class Entrepot
 {
     #[ORM\Id]
